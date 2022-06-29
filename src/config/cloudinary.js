a5d986dc4ba6d35676571d4e8ed0878017588df7
Ivan Yura Takano <ivanyurat@gmail.com>
2022-06-29 20:55:15 -0300

    adicionando config.js

---
+++ src/config/cloudinary.js
@@ -0,0 +1,29 @@
+const cloudinary = require('cloudinary');
+require('dotenv').config();
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_API_SECRET, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_CLOUD_NAME, 
+  });
+
+exports.uploads = (file, folder ) => {
+
+    return new Promise(
+        (resolve) => {
+            cloudinary.uploader.upload(
+                file,
+                (cloudinaryReturn) => {
+                    resolve({
+                        imageUrl: cloudinaryReturn.url
+                    })
+                },
+                {
+                    folder: folder,
+                    resource_type: "auto"
+                }
+            )
+        }        
+    )    
+};

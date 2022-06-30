718c82ba30bd4a966faadd2d54303af781fa80b2
Ivan Yura Takano <ivanyurat@gmail.com>
2022-06-30 11:20:00 -0300

    manutencao do cloudinary

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,28 +1,29 @@
-const cloudinary = require("cloudinary");
-require('dotenv').config()
+const cloudinary = require('cloudinary');
+require('dotenv').config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
-//metodo que vai executar o upload no cloudinary
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET, 
+  });
 
-exports.uploads = (file, folder) => {
-  return new Promise(
-    (resolve) => {
-        cloudinary.uploader.upload(
-            file,
-            (cloudinaryReturn) => {
-                resolve({
-                    imageUrl:cloudinaryReturn.url
-                })
-            },
-            {
-                folder: folder,
-                resource_type:"auto"
-            }
-        )
-    })
+exports.uploads = (file, folder ) => {
+
+    return new Promise(
+        (resolve) => {
+            cloudinary.uploader.upload(
+                file,
+                (cloudinaryReturn) => {
+                    resolve({
+                        imageUrl: cloudinaryReturn.url
+                    })
+                },
+                {
+                    folder: folder,
+                    resource_type: "auto"
+                }
+            )
+        }        
+    )    
 };
